a49352187ae70bee511c958349068018c080243b
hamzabely1 <hamzaciao80@gmail.com>
2022-07-01 21:00:18 +0200

    m

--- src/App.js
+++ src/App.js
@@ -10,11 +10,10 @@ import Admin from './page/PageAdmin/Create';
 
 import Legumes from './page/Legumes';
 import Panier from './page/Panier';
-<<<<<<< HEAD
+
 import Home_admin from './page/PageAdmin/Home_admin';
-let role = localStorage.getItem('role')
-=======
-import Home_admin from './page/PageAdmin/Crud_admin';
+
+
 import Edit_admin from './page/PageAdmin/Edit_admin'
 import Plus_sur_taz from './page/Plus_sur_taz';
 import Paiment from './page/Paiment';
@@ -22,7 +21,6 @@ import AdminPrivateRoute from './page/PageAdmin/AdminPrivateRoute';
 import Email from '../src/component/Email'
 import RouteAdmin from './route/RouteAdmin'
 import * as Cookie from '../src/connexion/Cookie'
->>>>>>> ff51fcc9801a93e0b9df704b22a311b86d60f0e0
 
 
 function App() {
@@ -40,15 +38,12 @@ function App() {
 <Routes>
 <Route path='/' element={<Home/>}/>
 <Route path='/home' element={<Home/>}/>
-
-<<<<<<< HEAD
 <Route path='fruits' element={<Fruits/>}/>
 <Route path='legumes' element={<Legumes/>}/>
 <Route path='panier' element={<Panier/>}/>
 
 <Route path="login" element={localStorage.getItem('nom') ?<Home/> :<Login/>}/>
 <Route path="register" element={localStorage.getItem('nom') ?<Home/> :<Register/>}/>
-=======
 <Route path="fruits"element={<Fruits/>}/>
 <Route path="legumes" element={<Legumes/>}/>
 <Route path="panier"element={ Cookie.getCookie('token') ?<Panier/> :<Home/>}/>
@@ -58,9 +53,7 @@ function App() {
 <Route path="login" element={<Login/>}/>
 <Route path="register" element={<Register/>}/>
 <Route path="admin" element={<Create_admin/>}/>
->>>>>>> ff51fcc9801a93e0b9df704b22a311b86d60f0e0
 
-<Route path="admin" element={localStorage.getItem('role') ?<Admin/> :<Home/>}/>
 <Route path="register" element={localStorage.getItem('role') ?<Home/> :<Register/>}/>
 <Route path="login" element={localStorage.getItem('role') ?<Home/> :<Login/>}/>
 <Route path="home_admin" element={<Home_admin/>}/>
